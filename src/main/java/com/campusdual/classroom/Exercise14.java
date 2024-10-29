43f3a9e66c3eebde8fb77b0e50c0d5ec40fd4a53
Alejandro Martín Millarengo <alexmarmill@gmail.com>
2024-10-29 19:19:20 +0100

    Exercise 14

--- src/main/java/com/campusdual/classroom/Exercise14.java
+++ src/main/java/com/campusdual/classroom/Exercise14.java
@@ -7,6 +7,15 @@ public class Exercise14 {
         Teacher t = new Teacher("Maria", "Montessori", "Educación");
         PoliceOfficer po = new PoliceOfficer("Jake", "Peralta", "B-99");
         Doctor d = new Doctor("Gregory", "House", "Nefrología e Infectología");
+        System.out.println("Detalles de la persona: ");
+        p.getDetails();
+        System.out.println("Detalles del oficial de policia: ");
+        po.getDetails();
+        System.out.println("Detalles del profesor: ");
+        t.getDetails();
+        System.out.println("Detalles del doctor: ");
+        d.getDetails();
+        }
+
     }
 
-}
--- src/main/java/com/campusdual/classroom/PoliceOfficer.java
+++ src/main/java/com/campusdual/classroom/PoliceOfficer.java
@@ -8,4 +8,9 @@ public class PoliceOfficer extends Person {
 		super(name, surname);
 		this.squad = squad;
 	}
+	public void getDetails() {
+		System.out.println("Nombre: " + name);
+		System.out.println("Apellido : " + surname);
+		System.out.println("Grupo: " + squad);
+	}
 }
